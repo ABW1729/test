7a66ed8999e5e095f8b50d298b6f545d86ca648d
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-17 18:06:38 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,8 @@
 const config = {
-  apiUrl: 'http://34.227.101.23:8000'
+  login: 'http://34.227.101.23:8000/api/login',
+  register:'http://34.227.101.23:8000/api/register',
+  add:'http://34.227.101.23:8000/api/add',
+  delete:'http://34.227.101.23:8000/api/delete'
 };
 
 export default config;
