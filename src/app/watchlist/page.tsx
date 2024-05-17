72004da7b2e0e8a61a824d3f89bf6d6012cbcaab
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-17 14:55:38 +0530

    Update page.tsx

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Checkbox,Table,Typography, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import Navbar from '../components/Navbar';
-import Header from '../components/Header/page'
 import { getCookie } from 'cookies-next';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
