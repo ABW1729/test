8bcb786e5ef6e0cd04ed8bb35401d7c0fefec176
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-17 19:06:23 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@
 import {getCookie} from "cookies-next"
 import { useRouter } from 'next/navigation'
 export const redirect = () => {
+    const router = useRouter();
   const token = getCookie('token');
   if (!token) {
    router.replace("/");
