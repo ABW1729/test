bfdab743c3c1fc990fc963704daaef82d8c5ae02
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-29 18:55:08 +0530

    Update urls.py

--- backend/backend/djangoapp/urls.py
+++ backend/backend/djangoapp/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from . import views
-urlpatterns=[path('',views.index,name='index'),
- path('api/add', views.add_stock, name='add_stock'),
- path('api/delete', views.delete_stock, name='delete_stock'),
- path('api/check_token',views.check_token,name='check_token'),
-  path('api/login',views.login_view,name='login'),
-   path('api/logout',views.logout_view,name='logout'),
-   path('api/register',views.signup_view,name='signup'),
-   path('api/stocks',views.get_stock,name='stock'),
-   
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RefreshTokenView,TokenCheckView,SignupView, LoginView,LogoutView, StockPriceView, GetStockView, AddStockView, DeleteStockView
+from .token  import CustomTokenObtainPairView
+
+urlpatterns = [
+   path('api/refresh',RefreshTokenView.as_view(),name='refresh'),
+    path('api/check',TokenCheckView.as_view(),name='check'),
+    path('api/register', SignupView.as_view(), name='signup'),
+    path('api/login', LoginView.as_view(), name='login'),
+    path('api/token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+   path('api/logout', LogoutView.as_view(), name='logout'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/price', StockPriceView.as_view(), name='stock_price'),
+    path('api/stocks', GetStockView.as_view(), name='get_stock'),
+    path('api/add', AddStockView.as_view(), name='add_stock'),
+    path('api/delete', DeleteStockView.as_view(), name='delete_stock'),
+
 ]
