356ba3b211418176e5677d23238f776a622c03a5
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-05-29 18:57:38 +0530

    Update models.py

--- backend/backend/djangoapp/models.py
+++ backend/backend/djangoapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-class User(AbstractBaseUser):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    stocks = JSONField(default=dict)
+
+class BlacklistedToken(models.Model):
+    jti = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.jti
